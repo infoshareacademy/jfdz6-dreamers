818da3397e9c4ceb1734c3849b7688740e76dd68
Zetriax <zetriax.b3@gmail.com>
2017-08-06 12:49:23 +0200

    Image to cookies add

--- js/Cookies.js
+++ js/Cookies.js
@@ -13,7 +13,7 @@
 
     container.setAttribute('id', 'cookieinfo');
     container.setAttribute('class', 'cookie-alert');
-    container.innerHTML = '<h6>Ta strona wykorzystuje pliki cookie</h6><p>Używamy informacji zapisanych za pomocą plików cookies w celu zapewnienia maksymalnej wygody w korzystaniu z naszego serwisu. Mogą też korzystać z nich współpracujące z nami firmy badawcze oraz reklamowe. Jeżeli wyrażasz zgodę na zapisywanie informacji zawartej w cookies kliknij na &bdquo;x&rdquo; w prawym górnym rogu tej informacji. Jeśli nie wyrażasz zgody, ustawienia dotyczące plików cookies możesz zmienić w swojej przeglądarce.</p>';
+    container.innerHTML = '<h6>Ta strona wykorzystuje pliki cookie <img src="img/cookies.png" height="50" width="50"/></h6><p>Używamy informacji zapisanych za pomocą plików cookies w celu zapewnienia maksymalnej wygody w korzystaniu z naszego serwisu. Mogą też korzystać z nich współpracujące z nami firmy badawcze oraz reklamowe. Jeżeli wyrażasz zgodę na zapisywanie informacji zawartej w cookies kliknij na &bdquo;x&rdquo; w prawym górnym rogu tej informacji. Jeśli nie wyrażasz zgody, ustawienia dotyczące plików cookies możesz zmienić w swojej przeglądarce.</p>';
 
     link.setAttribute('href', '#');
     link.setAttribute('title', 'Zamknij');
