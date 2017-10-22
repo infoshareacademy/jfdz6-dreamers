c7083d3b5c67c56267eac888bf3c0841d44c27c8
Andrzej Puzynski <andrzejpuzynski@tlen.pl>
2017-10-22 13:00:06 +0200

    Add four party places

--- js/AndrewGame.js
+++ js/AndrewGame.js
@@ -1,3 +1,41 @@
 $(function() {
 
+
+    var $board = $('#board');
+    var $table = $('<table>');
+    var $tr = $('<tr>');
+    var $td;
+
+    function createTable(width, height) {
+        for (var j = 0; j < height; j++) {
+            $tr = $('<tr>');
+            for (var i = 0; i < width; i++) {
+                $td = $('<td>');
+                $tr.append($td);
+            }
+            $table.append($tr);
+        }
+    }
+
+    createTable(20, 20);
+    $('td', $table).addClass('cell');
+    $board.append($table);
+
+    var snakeCells = $('td').slice(0, 1).toArray();
+    $(snakeCells).addClass('snake');
+
+
+    function drawParty(x, y) {
+        for (var i = x; i < x + 3; i++) {
+            for (var j = y; j < y + 3; j++) {
+                $('table tr:nth-child(' + i + ') td:nth-child(' + j + ')').addClass('party')
+            }
+        }
+    }
+
+    $partyOne = drawParty(5, 5);
+    $partyTwo = drawParty(5, 14);
+    $partyThree = drawParty(14, 5);
+    $partyFour = drawParty(14, 14);
+
 });
